69a87f907f94d67772dfe8692ae67580e618620f
Aashish Panthi <aashishpanthi11@gmail.com>
2023-02-04 12:34:01 +0545

    add expense model

---
+++ ExpenseTrackerRestAPI/ExpenseTrackerRestAPI/Models/Expense.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ExpenseTrackerRestAPI.Models
+{
+    public class Expense
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Type { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
