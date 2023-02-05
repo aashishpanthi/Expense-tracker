f3d6e6ae77b173dd0d2eaf0f64cf667d78272173
Aashish Panthi <aashishpanthi11@gmail.com>
2023-02-05 20:38:03 +0545

    ADD: Expense controller

--- ExpenseTrackerRestAPI/ExpenseTrackerRestAPI/Models/Expense.cs
+++ ExpenseTrackerRestAPI/ExpenseTrackerRestAPI/Models/Expense.cs
@@ -4,9 +4,8 @@ namespace ExpenseTrackerRestAPI.Models
 {
     public class Expense
     {
-        public int Id { get; set; }
+        public Guid Id { get; set; }
 
-        [Required]
         public string Name { get; set; }
         public string Description { get; set; }
         public string Type { get; set; }
