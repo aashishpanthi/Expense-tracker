87ce135035e44ffef5821db9f56864aaf1f6a0ad
Aashish Panthi <aashishpanthi11@gmail.com>
2023-02-05 20:52:31 +0545

    update expense model

--- ExpenseTrackerRestAPI/ExpenseTrackerRestAPI/Models/Expense.cs
+++ ExpenseTrackerRestAPI/ExpenseTrackerRestAPI/Models/Expense.cs
@@ -4,11 +4,11 @@ namespace ExpenseTrackerRestAPI.Models
 {
     public class Expense
     {
-        public Guid Id { get; set; }
+        public Guid Id { get; init; }
 
         public string Name { get; set; }
         public string Description { get; set; }
         public string Type { get; set; }
-        public DateTime Date { get; set; }
+        public DateTime Date { get; init; }
     }
 }
