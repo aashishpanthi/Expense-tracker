0990bad69309f056d9eeb706f4035b3142c7cb6f
roshan-acharya <roshanacharya9808@gmail.com>
2023-02-10 21:42:14 +0545

    login register

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,7 +1,109 @@
-import React from "react";
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import { Link } from "react-router-dom";
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
-function Login() {
-  return <div>Login</div>;
+import GoogleIcon from "@mui/icons-material/Google";
+
+
+const theme = createTheme();
+
+
+export default function Login() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      email: data.get('email'),
+      password: data.get('password'),
+    });
+    
+  };
+  
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 7,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          
+          <Typography component="h1" variant="h4">
+            Sign in
+          </Typography>
+          <div className='google'>
+          <Button
+            variant="outlined"
+            color="primary"
+            fullWidth
+            sx={{
+              mt: 3,
+              mb: 1,
+            }}
+          >
+            <GoogleIcon
+              sx={{
+                mr: 3,
+              }}
+            />{" "}
+            Sign in With Google
+          </Button>
+          </div>
+          <span className='or'>OR</span>
+          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign In
+            </Button>
+            
+            <Grid container>
+              
+              <Grid item>
+                <Link to="/register" className='txt'>
+                  {"Don't have an account? Sign Up"}
+                </Link>
+              </Grid>
+            </Grid>
+            
+          </Box>
+        </Box>
+      </Container>
+    </ThemeProvider>
+  );
 }
-
-export default Login;
