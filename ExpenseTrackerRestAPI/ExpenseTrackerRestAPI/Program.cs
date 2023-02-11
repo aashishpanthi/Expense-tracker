853b743d651f80c74d7caac69999eb89088d4c5c
Aashish Panthi <aashishpanthi11@gmail.com>
2023-02-11 16:41:36 +0545

    ADD: port number

--- ExpenseTrackerRestAPI/ExpenseTrackerRestAPI/Program.cs
+++ ExpenseTrackerRestAPI/ExpenseTrackerRestAPI/Program.cs
@@ -15,6 +15,7 @@ namespace ExpenseTrackerRestAPI
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
+                    webBuilder.UseUrls("http://0.0.0.0:5000");
                 });
     }
 }
