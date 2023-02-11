61619b972d272548d0cffc24e8918e896562790a
roshan-acharya <roshanacharya9808@gmail.com>
2023-02-11 18:35:05 +0545

    ok

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,6 +2,10 @@ import React from 'react'
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <section className='home_section'>
+      <div className='heading'><h1 className='heading1'>Expense</h1><h1 className='heading2'>Tracker</h1></div>
+      <div><p></p></div>
+      <div><button></button></div>
+    </section>
   )
 }
