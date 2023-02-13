f383099a161615324c6f6d19685b241c40493c28
Aashish Panthi <aashishpanthi11@gmail.com>
2023-02-13 20:13:24 +0545

    UPDATE: API endpoint

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:5000/",
-  timeout: 2000,
+  baseURL: "http://64.227.164.205:5000/",
+  timeout: 1000,
+  headers: { "Content-type": "application/json" },
 });
 
 export default instance;
