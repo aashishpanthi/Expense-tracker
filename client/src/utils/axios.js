08c602ca993a00e548844330001670e4444e4b21
Aashish Panthi <aashishpanthi11@gmail.com>
2023-02-13 20:15:51 +0545

    Increase timeout

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const instance = axios.create({
   baseURL: "http://64.227.164.205:5000/",
-  timeout: 1000,
+  timeout: 5000,
   headers: { "Content-type": "application/json" },
 });
 
